9b8794226bb38c5cb0db8aed5b732cb573bd8430
tejus <tejussharma5252@gmail.com>
2025-05-07 12:11:49 +0530

    added 2 test files

---
+++ tests/example.spec.ts
@@ -0,0 +1,17 @@
+import { test,expect } from '@playwright/test';
+
+test('Test 1' , async({page})=>{
+await page.goto("https://saucedemo.com");
+console.log("Reached the Page");
+await expect(page).toHaveTitle('Swag Labs');
+})
+
+test('Test 2' , async({page})=>{
+    await page.goto("https://saucedemo.com");
+    console.log("Reached the Page - 2");
+    await expect(page).toHaveTitle('Swag Labs');
+    console.log("123");
+    console.log("234");
+})
+    
+
